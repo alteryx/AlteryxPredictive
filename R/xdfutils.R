6aa49b01b137f82df738522446e1cc0520fda36c
Dylan Blanchard <dylblanchard@gmail.com>
2016-08-31 10:51:40 -0500

    add xdf utility functions

---
+++ R/xdfutils.R
@@ -0,0 +1,42 @@
+#' extract levels from xdf data
+#'
+#' @param form R formula
+#' @param xdf path to xdf file
+#' @return names of levels - vector
+#' @export
+getXdfLevels <- function(form, xdf) {
+  factors <- rxSummary(form, data = xdf)$categorical
+  the_names <- sapply(factors, function(x) names(x)[1])
+  if (length(the_names) == 1) {
+    the_levels <- eval(parse(text = paste("list(", the_names, " = as.character(factors[[1]][[1]]))")))
+  } else {
+    the_levels <- sapply(factors, function(x) as.character(x[[1]]))
+    names(the_levels) <- the_names
+  }
+  the_levels
+}
+
+#' extract xdf properties from incoming xdf data
+#'
+#' @param input_name name of incoming connection
+#' @return list with boolean is_XDF and string xdf_path representing
+#'  whether data is XDF and the path of the data
+#' @import AlteryxRDataX
+#' @import rjson
+#' @export
+getXdfProperties <- function(input_name) {
+  is_XDF <- FALSE
+  meta_data <- AlteryxRDataX::read.AlteryxMetaInfo(input_name)
+  the_source <- as.character(meta_data$Source)
+  if (all(substr(the_source, 3, 9) == "Context")) {
+    library(rjson)
+    context_list <- rjson::fromJSON(the_source[1])
+    if (context_list$Context == "XDF") {
+      is_XDF <- TRUE
+      xdf_path <- context_list$File.Loc
+      list(is_XDF = is_XDF, xdf_path = xdf_path)
+    } else {
+      stop.Alteryx("At this time only XDF scaling is supported.")
+    }
+  }
+}
