dc85409cb461829974cf39066c85c0db5df6d3e8
Dylan Blanchard <dylblanchard@gmail.com>
2016-09-20 16:59:36 -0500

    add error testing for checkValidConfig

--- tests/testthat/test-checkValidConfig.R
+++ tests/testthat/test-checkValidConfig.R
@@ -11,3 +11,18 @@ test_that("proper warning is returned for (and only for) < 5 numeric target vals
   expect_that(checkValidConfig(config, the.data5, names, is_XDF),
               gives_warning(NA))
 })
+
+test_that("proper error throwing is done for invalid control parameter", {
+  is_XDF = FALSE
+  the.data <- data.frame(y = 1:100)
+  names <- list(y = "y")
+  config0 <- list(cp = 0)
+  config1 <- list(cp = 1)
+  configblah <- list(cp = "blah")
+  expect_that(checkValidConfig(config0, the.data, names, is_XDF),
+              throws_error("The complexity parameter must be between 0 and 1. Please try again."))
+  expect_that(checkValidConfig(config1, the.data, names, is_XDF),
+              throws_error("The complexity parameter must be between 0 and 1. Please try again."))
+  expect_that(checkValidConfig(configblah, the.data, names, is_XDF),
+              throws_error("The complexity parameter provided is not a number. Please enter a new value and try again."))
+})
