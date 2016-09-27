cf1a7df4c3f60e1f1a16392772fb1a86c9c5048a
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2016-09-27 10:29:01 -0700

    update logistic regression tests.

--- tests/testthat/test-integration-logisticRegression.R
+++ tests/testthat/test-integration-logisticRegression.R
@@ -22,48 +22,25 @@ inputs <- list(
 
 
 #' ### Run and Create Outputs
-runLogisticRegression <- function(inputs, config){
-  library(car)
+exp_logistic_model <- glm(
+  admit ~ gre + gpa + rank, data = admission,
+  family = binomial(logit)
+)
 
-  #' Modify the link so that it can be passed on to R.
-  if (config$the.link == "complementary log-log"){
-    config$the.link <- "cloglog"
-  }
-
-  if (inputs$XDFInfo$is_XDF){
-    d <- processLogisticXDF(inputs, config)
-    glm.out <- createReportLogisticXDF(d$the.model, config, d$null.model)
-    plot.out <- function(){createPlotOutputsLogisticXDF()}
-  } else {
-    d <- processLogisticOSR(inputs, config)
-    glm.out <- createReportLogisticOSR(d$the.model, config, d$model_type)
-    plot.out <- function(){
-      createPlotOutputsLogisticOSR(d$the.model, FALSE, config)
-    }
-  }
-
-  # Report Output
-  write.Alteryx2(glm.out, nOutput = 1)
-
-  # Plot Output
-  whr <- graphWHR2(inches = TRUE, in.w = 6, in.h = 6, config$graph.resolution)
-  AlteryxGraph2(plot.out(), 2, width = whr[1], height = whr[2],
-                res = whr[3], pointsize = 9)
-
-  # Model Output
-  the.obj <- prepModelForOutput(config$model.name, d$the.model)
-  write.Alteryx2(the.obj, nOutput = 3)
-}
-
-
-test_that("admission data with logit link", {
-  result <- runLogisticRegression(inputs, config)
-  expect_lt(abs(result$Object[[1]]$coefficients["gpa"] - 0.7770136), 10e-6)
+test_that("admission data with logit link returns correct coefficients", {
+  result <- AlteryxPredictive:::runLogisticRegression(inputs, config)
+  expect_equal(
+    result$Object[[1]]$coefficients,
+    exp_logistic_model$coefficients
+  )
 })
 
-
-test_that("admission data with probit link", {
+test_that("admission data with probit link returns correct coefficients", {
   config$the.link <- "probit"
-  result <- runLogisticRegression(inputs, config)
-  expect_lt(abs(result$Object[[1]]$coefficients["gpa"] - 0.464360), 10e-6)
+  exp_probit_model <- update(exp_logistic_model, family = binomial(probit))
+  result <- AlteryxPredictive:::runLogisticRegression(inputs, config)
+  expect_equal(
+    result$Object[[1]]$coefficients,
+    exp_probit_model$coefficients
+  )
 })
