dc82edec2785fcdcc3fe2cf9c953d1bd60f7230b
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2016-09-27 15:13:30 -0700

    move runLinearRegression to runModels

--- R/runModels.R
+++ R/runModels.R
@@ -30,3 +30,31 @@ runLogisticRegression <- function(inputs, config){
   write.Alteryx2(the.obj, nOutput = 3)
   return(the.obj)
 }
+
+runLinearRegression <- function(inputs, config){
+  library(car)
+  config$model.name = validName(config$model.name)
+  if (inputs$XDFInfo$is_XDF){
+    the.model <- processLinearXDF(inputs, config)
+    lm.out <- createReportLinearXDF(the.model, config)
+    plot.out <- function(){createPlotOutputsXDF()}
+  } else {
+    the.model <- processLinearOSR(inputs, config)
+    lm.out <- createReportLinearOSR(the.model, config)
+    plot.out <- function(){createPlotOutputsLinearOSR(the.model)}
+  }
+
+  # Report
+  write.Alteryx2(lm.out, nOutput = 1)
+
+  # Plot Output
+  # whr <- graphWHR(inches = "True", in.w = 6, in.h = 6, config$resolution)
+  whr <- AlteryxPredictive:::graphWHR2(inches = TRUE, in.w = 6, in.h = 6,
+                                       config$graph.resolution)
+  AlteryxGraph2(plot.out(), 2, width = whr[1], height = whr[2],
+                res = whr[3], pointsize = 9)
+
+  # Model Object
+  the.obj <- prepModelForOutput(config$model.name, the.model)
+  write.Alteryx2(the.obj, nOutput = 3)
+}
