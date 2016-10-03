aa44301432fc46cdbea1220e011fac9e8525f190
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2016-10-03 09:14:19 -0700

    add initial xdf test for linear regression.

---
+++ tests/testthat/test-integration-linear-regression-xdf.R
@@ -0,0 +1,28 @@
+context("integration-linear-regression-xdf")
+
+config <- list(
+  `graph.resolution` = '1x',
+  `Model Name` = 'My Regression',
+  `Omit Constant` = FALSE,
+  `Use Weights` = FALSE,
+  `X Vars` = names(mtcars)[-1],
+  `Y Var` = 'mpg'
+)
+
+inputs <- list(
+  the.data = mtcars,
+  XDFInfo = list(
+    is_XDF = TRUE,
+    xdf_path = system.file('xdfdata/mtcars.xdf',
+      package = 'AlteryxPredictive')
+  )
+)
+
+test_that('linear regression works correctly on mtcars', {
+  results <- AlteryxPredictive:::runLinearRegression(inputs, config)
+  exp_model <- lm(mpg ~ ., data = mtcars)
+  expect_equal(
+    results$Object[[1]]$coefficients[,1],
+    exp_model$coefficients
+  )
+})
