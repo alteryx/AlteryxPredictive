c5383f7c13090a3089c42ed4f5de1d64cd5d43eb
kuol <liukuo99@gmail.com>
2016-10-20 15:33:43 -0600

    convert writeOuputs to S3 methods.

--- R/runModels.R
+++ R/runModels.R
@@ -1,4 +1,8 @@
-writeOutputs <- function(results){
+writeOutputs <- function(results, ...) {
+  UseMethod('writeOutputs')
+}
+
+writeOutputs.GLM <- function(results){
   # Report
   write.Alteryx2(results$report, nOutput = 1)
 
@@ -14,53 +18,7 @@ writeOutputs <- function(results){
   write.Alteryx2(the.obj, nOutput = 3)
 }
 
-getResultsLogisticRegression <- function(inputs, config){
-  library(car)
-  # Modify the link so that it can be passed on to R.
-  if (config$Link == "complementary log-log"){
-    config$Link <- "cloglog"
-  }
-
-  if (inputs$XDFInfo$is_XDF){
-    d <- processLogisticXDF(inputs, config)
-    glm.out <- createReportLogisticXDF(d$the.model, config, d$null.model)
-    plot.out <- function(){createPlotOutputsLogisticXDF()}
-  } else {
-    d <- processLogisticOSR(inputs, config)
-    glm.out <- createReportLogisticOSR(d$the.model, config, d$model_type)
-    plot.out <- function(){
-      createPlotOutputsLogisticOSR(d$the.model, FALSE, config)
-    }
-  }
-  list(model = d$the.model, report = glm.out, plot = plot.out)
-}
-runLogisticRegression <- function(inputs, config){
-  results <- getResultsLogisticRegression(inputs, config)
-  writeOutputs(results)
-}
-
-getResultsLinearRegression <- function(inputs, config){
-  library(car)
-  config$`Model Name`= validName(config$`Model Name`)
-  if (inputs$XDFInfo$is_XDF){
-    the.model <- processLinearXDF(inputs, config)
-    lm.out <- createReportLinearXDF(the.model, config)
-    plot.out <- function(){createPlotOutputsXDF()}
-  } else {
-    the.model <- processLinearOSR(inputs, config)
-    lm.out <- createReportLinearOSR(the.model, config)
-    plot.out <- function(){createPlotOutputsLinearOSR(the.model)}
-  }
-  list(model = the.model, report = lm.out, plot = plot.out)
-}
-
-runLinearRegression <- function(inputs, config){
-  results <- getResultsLinearRegression(inputs, config)
-  writeOutputs(results)
-}
-
-
-writeOutputs.decisionTree <- function(results) {
+writeOutputs.DecisionTree <- function(results) {
   # Report Output
   write.Alteryx2(results$report, nOutput = 1)
 
@@ -86,6 +44,60 @@ writeOutputs.decisionTree <- function(results) {
   AlteryxRviz::renderInComposer(results$dashboard, nOutput = 5)
 }
 
+# Logistic Regression ----
+getResultsLogisticRegression <- function(inputs, config){
+  library(car)
+  # Modify the link so that it can be passed on to R.
+  if (config$Link == "complementary log-log"){
+    config$Link <- "cloglog"
+  }
+
+  if (inputs$XDFInfo$is_XDF){
+    d <- processLogisticXDF(inputs, config)
+    glm.out <- createReportLogisticXDF(d$the.model, config, d$null.model)
+    plot.out <- function(){createPlotOutputsLogisticXDF()}
+  } else {
+    d <- processLogisticOSR(inputs, config)
+    glm.out <- createReportLogisticOSR(d$the.model, config, d$model_type)
+    plot.out <- function(){
+      createPlotOutputsLogisticOSR(d$the.model, FALSE, config)
+    }
+  }
+  results <- list(model = d$the.model, report = glm.out, plot = plot.out)
+  class(results) <- "GLM"
+  results
+}
+runLogisticRegression <- function(inputs, config){
+  results <- getResultsLogisticRegression(inputs, config)
+  writeOutputs(results)
+}
+
+# Linear Regression ----
+getResultsLinearRegression <- function(inputs, config){
+  library(car)
+  config$`Model Name`= validName(config$`Model Name`)
+  if (inputs$XDFInfo$is_XDF){
+    the.model <- processLinearXDF(inputs, config)
+    lm.out <- createReportLinearXDF(the.model, config)
+    plot.out <- function(){createPlotOutputsXDF()}
+  } else {
+    the.model <- processLinearOSR(inputs, config)
+    lm.out <- createReportLinearOSR(the.model, config)
+    plot.out <- function(){createPlotOutputsLinearOSR(the.model)}
+  }
+
+  results <- list(model = the.model, report = lm.out, plot = plot.out)
+  class(results) <- "GLM"
+  results
+}
+
+runLinearRegression <- function(inputs, config){
+  results <- getResultsLinearRegression(inputs, config)
+  writeOutputs(results)
+}
+
+
+# Decision Tree ----
 getResultsDecisionTree <- function(inputs, config) {
   # Set the seed to get run-over-run consistency
   set.seed(1)
@@ -107,12 +119,14 @@ getResultsDecisionTree <- function(inputs, config) {
   makePrunePlot <- function(){createPrunePlotDT(the.model)}
   dashboard <- createDashboardDT(the.model, inputs$XDFInfo$is_XDF)
 
-  list(model = the.model, report = the.report,
-       treePlot = makeTreePlot, prunePlot = makePrunePlot,
-       dashboard = dashboard)
+  results <- list(model = the.model, report = the.report,
+                  treePlot = makeTreePlot, prunePlot = makePrunePlot,
+                  dashboard = dashboard)
+  class(results) <- "DecisionTree"
+  results
 }
 
 runDecisionTree <- function(inputs, config){
   results <- getResultsDecisionTree(inputs, config)
-  writeOutputs.decisionTree(results)
+  writeOutputs(results)
 }
