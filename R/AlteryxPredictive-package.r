c4c119deeebc27b156bd5afaa05d806993d3b4c8
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2016-10-21 14:04:46 -0700

    add imports from recommended pkgs.

---
+++ R/AlteryxPredictive-package.r
@@ -0,0 +1,11 @@
+#' AlteryxPredictive.
+#'
+#' @name AlteryxPredictive
+#' @docType package
+#' @importFrom grDevices dev.off palette
+#' @importFrom graphics arrows axis box dotchart legend par plot points text title
+#' @importFrom stats as.formula binomial complete.cases family formula glm lm
+#'   median na.omit pf predict qt quasibinomial runif sd setNames
+#' @importFrom utils capture.output head install.packages installed.packages
+#'   modifyList packageVersion
+NULL
