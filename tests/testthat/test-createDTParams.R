384dbd731753bdabd0f8f507af37ab1953a970e5
Dylan Blanchard <dylblanchard@gmail.com>
2016-12-12 09:19:14 -0600

    update createDTParams tests

--- tests/testthat/test-createDTParams.R
+++ tests/testthat/test-createDTParams.R
@@ -11,6 +11,18 @@ config <- list(
   minbucket = 1,
   xval = 1,
   maxdepth = 1,
+  trials = 1,
+  rules = FALSE,
+  subset = FALSE,
+  bands = 0,
+  bands.check = FALSE,
+  winnow = FALSE,
+  CF = .25,
+  minCases = 2,
+  fuzzyThreshold = FALSE,
+  sample = 0,
+  seed = 1,
+  earlyStopping = TRUE,
   cp = .01,
   used.weights = TRUE,
   select.type = TRUE,
@@ -20,10 +32,10 @@ config <- list(
   usesurrogate.1 = TRUE,
   usesurrogate.2 = FALSE,
   method = "class",
-  maxNumBins = 4
+  maxNumBins = 4,
+  total.correct = TRUE,
+  GlobalPruning = TRUE
 )
-
-class(config) <- "rpart"
 
 test_that("non XDF results give expected param list", {
   xdf_properties <- list(
@@ -37,6 +49,17 @@ test_that("non XDF results give expected param list", {
       minbucket = 1,
       xval = 1,
       maxdepth = 1,
+      trials = 1,
+      rules = FALSE,
+      subset = FALSE,
+      bands = 0,
+      winnow = FALSE,
+      CF = .25,
+      minCases = 2,
+      fuzzyThreshold = FALSE,
+      sample = 0,
+      seed = 1,
+      earlyStopping = TRUE,
       cp = .01,
       data = quote(the.data),
       formula = formula("y ~ a + b + c"),
@@ -44,33 +67,9 @@ test_that("non XDF results give expected param list", {
       method = "class",
       parms = list(split = "gini"),
       usesurrogate = 1,
-      maxNumBins = 4
-    )
-  ))
-})
-
-class(config) <- "XDF"
-
-test_that("XDF results give expected param list", {
-  xdf_properties <- list(
-    is_XDF = TRUE,
-    xdf_path = "xdf"
-  )
-
-  expect_that(createDTParams(config, names), is_equivalent_to(
-    list(
-      minsplit = 1,
-      minbucket = 1,
-      xval = 1,
-      maxdepth = 1,
-      cp = .01,
-      data = quote(the.data),
-      formula = formula("y ~ a + b + c"),
-      weights = c("w"),
-      method = "class",
-      parms = list(split = "gini"),
-      usesurrogate = 1,
-      maxNumBins = 4
+      maxNumBins = 4,
+      noGlobalPruning = FALSE,
+      surrogatestyle = 0
     )
   ))
 })
