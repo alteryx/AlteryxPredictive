d02efd2e310a71a7dac384ab66a7557483b042c9
btoomey <bridget.9216@gmail.com>
2016-12-16 11:09:49 -0700

    Add param tag to getYVar.

--- R/getYVar.R
+++ R/getYVar.R
@@ -9,13 +9,14 @@ getYVar <- function(mod) {
 }
 
 # Get y variable from model object
+#' @param mod model object
 #' @export
 #' @rdname getYVar
 getYVar.default <- function(mod){
   as.character(formula(mod))[2]
 }
 
-
+#' @param mod model object
 #' @export
 #' @rdname getYVar
 getYVar.elnet <- function(mod) {
@@ -25,12 +26,14 @@ getYVar.elnet <- function(mod) {
 #Note that this will need to be updated once regularized logistic regression
 #is finished. We can put an if statement to check if mod$glmnet.fit is elnet
 #or lognet and proceed accordingly.
+#' @param mod model object
 #' @export
 #' @rdname getYVar
 getYVar.cv.glmnet <- function(mod) {
   mod$yvar
 }
 
+#' @param mod model object
 #' @export
 #' @rdname getYVar
 getYVar.naiveBayes <- function(mod) {
