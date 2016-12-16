bf8baf67c2c469b72eac76730477ed3ec46afb5b
btoomey <bridget.9216@gmail.com>
2016-12-16 11:33:21 -0700

    Remove rdname and param tags from individual methods and update documentation.

--- R/getYVar.R
+++ R/getYVar.R
@@ -3,22 +3,17 @@
 #' @param mod model object
 #' @export
 #' @author Ramnath Vaidyanathan, Bridget Toomey
-#' @rdname getYVar
 getYVar <- function(mod) {
   UseMethod('getYVar')
 }
 
 # Get y variable from model object
-#' @param mod model object
 #' @export
-#' @rdname getYVar
 getYVar.default <- function(mod){
   as.character(formula(mod))[2]
 }
 
-#' @param mod model object
 #' @export
-#' @rdname getYVar
 getYVar.elnet <- function(mod) {
   mod$yvar
 }
@@ -26,16 +21,12 @@ getYVar.elnet <- function(mod) {
 #Note that this will need to be updated once regularized logistic regression
 #is finished. We can put an if statement to check if mod$glmnet.fit is elnet
 #or lognet and proceed accordingly.
-#' @param mod model object
 #' @export
-#' @rdname getYVar
 getYVar.cv.glmnet <- function(mod) {
   mod$yvar
 }
 
-#' @param mod model object
 #' @export
-#' @rdname getYVar
 getYVar.naiveBayes <- function(mod) {
   mod$yvars
 }
