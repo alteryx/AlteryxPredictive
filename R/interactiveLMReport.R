3b5de9170858745c995b050dd420b96e2fa29280
Dylan Blanchard <dylblanchard@gmail.com>
2016-12-28 17:39:14 -0600

    add flightdeck import

--- R/interactiveLMReport.R
+++ R/interactiveLMReport.R
@@ -3,7 +3,7 @@
 #' @param config config
 #' @param data data (breaking the usual mold a little)
 #' @param model model
-#' @import MLmetrics DT
+#' @import MLmetrics DT flightdeck
 #' @export
 #' @author Todd Morley, Dylan Blanchard
 interactive_lm_report <- function(
