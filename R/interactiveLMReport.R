500c991556813ec608c8cdbe543847e6aa3df5a9
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2016-12-30 10:11:03 -0800

    move flightdeck to Suggests
    this was done to avoid dependencies between Alteryx packages. i will revisit this soon and move it back to imports once we can ensure that flightdeck will get built before AlteryxPredictive.

--- R/interactiveLMReport.R
+++ R/interactiveLMReport.R
@@ -3,7 +3,7 @@
 #' @param config config
 #' @param data data (breaking the usual mold a little)
 #' @param model model
-#' @import MLmetrics DT flightdeck
+#' @import MLmetrics DT
 #' @export
 #' @author Todd Morley, Dylan Blanchard
 interactive_lm_report <- function(
@@ -12,7 +12,7 @@ interactive_lm_report <- function(
   model
 ){
   # UI layout constants
-
+  require('flightdeck')
   totalWidth <- 12
   infoBoxWidth <- 6
   digits <- 3
