30286c8460932b5d51070808a9aaadae2ed2fe90
tmmorley <tmorley@alteryx.com>
2017-02-13 11:52:44 -0700

    file creation

---
+++ R/localization.R
@@ -0,0 +1,7 @@
+#' localization.R
+#
+################################################################################
+# This file contains all of the R functions necessary to localize Analytics
+# Products R code.
+################################################################################
+
